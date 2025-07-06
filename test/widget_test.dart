4bfd39fe8a0c60acbad9fbe3c35a4681b57d6552
noonkhallaf <62435145+noonkhallaf@users.noreply.github.com>
2025-07-06 11:08:46 +0300

    Create widget_test.dart

---
+++ test/widget_test.dart
@@ -0,0 +1,8 @@
+// تم الإنشاء للتوافق مع أدوات البناء التلقائي
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  testWidgets('Placeholder test', (WidgetTester tester) async {
+    expect(1 + 1, equals(2));
+  });
+}
