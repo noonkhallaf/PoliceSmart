e2d32fa2e661541181ed5e6f4b47ae9d0f31e4b5
noonkhallaf <62435145+noonkhallaf@users.noreply.github.com>
2025-07-06 05:34:43 +0300

    Create map_page.dart

---
+++ lib/map_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MapPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Map')),
+      body: const Center(
+        child: Text('Map functionality will appear here'),
+      ),
+    );
+  }
+}
